61e5db4e6e3cae090937634c3c303bf38aeab552
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-25 19:50:44 +0000

    point to localhost if no members

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,9 +4,23 @@
 # It sets variables according to platform.
 #
 class profile_haproxy::params {
-  $member_ips = $::member_ips
-  $member_names = $::member_names
+
+  if $::member_ips != undef {
+    $member_ips = split( $::member_ips, ',')
+  }
+  else {
+    $member_ips = 'localhost'
+  }
+
+  if $::member_names != undef {
+    $member_names = split( $::member_names, ',')
+  }
+  else {
+    $member_names = 'localhost'
+  }
+
   $monitor_address = $::monitor_address
+
   case $::osfamily {
     'Debian': {
       $package_name = 'profile_haproxy'
