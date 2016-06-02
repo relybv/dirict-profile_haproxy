5d0717e7da0693839a52c6d32dede87723072402
Paul Gomersbach <p.gomersbach@rely.nl>
2016-06-02 14:50:32 +0000

    add tests

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -26,19 +26,22 @@ describe 'profile_haproxy class' do
 
   
 # a profile class should test if the included packages and services are installed, enabled and running. Please adept to your needs. See examples below:
-#   describe package('ntp') do
-#      it { is_expected.to be_installed }
-#    end
-#
-#    describe service('ntp') do
-#      it { is_expected.to be_enabled }
-#      it { is_expected.to be_running }
-#    end
-#
-#    describe port(5432) do
-#      it { should be_listening.with('tcp') }
-#    end
-  
+   describe package('haproxy') do
+      it { is_expected.to be_installed }
+    end
+
+    describe service('haproxy') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+
+    describe port(80) do
+      it { should be_listening.with('tcp') }
+    end
+
+    describe port(443) do
+      it { should be_listening.with('tcp') }
+    end
 
   end
 end
