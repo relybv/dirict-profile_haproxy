db21bfc99f27ca072f72bf6ba80f844ae3b5d0e0
Paul Gomersbach <p.gomersbach@rely.nl>
2016-07-28 13:31:10 +0000

    prevent direct use of subclass

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,6 +3,10 @@
 # This class is called from profile_haproxy for service config.
 #
 class profile_haproxy::config {
+  # prevent direct use of subclass
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
 
   haproxy::listen { 'stats':
     ipaddress => '127.0.0.1',
--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,11 @@
 # This class is called from profile_haproxy for install.
 #
 class profile_haproxy::install {
+  # prevent direct use of subclass
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
+
   if $::monitor_address != undef {
     class { 'haproxy':
       global_options => {
