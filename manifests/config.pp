535a16d24447d91b1549532b1d3028fa567134f7
P. Gomersbach <p.gomersbach@rely.nl>
2016-09-08 10:31:16 +0000

    rewrite haproxy config

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,27 +17,32 @@ class profile_haproxy::config {
     },
   }
 
-  haproxy::listen { 'haproxy80':
+  # new http frontend
+  haproxy::frontend { 'haproxy00':
     collect_exported => false,
     ipaddress        => '*',
     ports            => '80',
     mode             => 'http',
     options          => {
-      'option'  => ['httplog'],
+      'default_backend' => 'appl80',
+      'balance'         => 'roundrobin',
+      'log'             => 'global',
+      'option'          => 'forwardfor',
+    },
+  }
+
+  # new http backend
+  haproxy::backend { 'appl80':
+    options => {
+      'option'  => [
+        'httplog',
+        'forwardfor',
+      ],
       'balance' => 'roundrobin',
     },
   }
 
-  haproxy::listen { 'haproxy443':
-    collect_exported => false,
-    ipaddress        => '*',
-    ports            => '443',
-    mode             => 'tcp',
-    options          => {
-      'balance' => 'source',
-    },
-  }
-
+  # old ok balancemember
   haproxy::balancermember { 'members80':
     listening_service => 'haproxy80',
     server_names      => $profile_haproxy::member_names,
@@ -46,6 +51,38 @@ class profile_haproxy::config {
     options           => 'check',
   }
 
+  # new https frontend
+  haproxy::frontend { 'haproxy443':
+    collect_exported => false,
+    mode             => 'http',
+    bind             => {
+      '*:443' => ['ssl', 'crt', '/etc/ssl/private/example.com.pem'],
+    },
+    options          => {
+      'option'                      => [
+        'http-keep-alive',
+        'forwardfor',
+      ],
+      'reqadd X-Forwarded-Proto:\\' => 'https',
+      'reqadd X-Forwarded-Port:\\'  => '443',
+      'default_backend'             => 'appl443',
+      'log'                         => 'global',
+      'stats'                       => 'enable',
+    },
+  }
+
+  # new https backend
+  haproxy::backend { 'appl443':
+    collect_exported => false,
+    mode             => 'http',
+    options          => {
+      'balance'                                   => 'source',
+      'http-request set-header X-Forwarded-Port'  => '%[dst_port]',
+      'http-request add-header X-Forwarded-Proto' => 'https',
+    },
+  }
+
+  # old ok balancemember
   haproxy::balancermember { 'members443':
     listening_service => 'haproxy443',
     server_names      => $profile_haproxy::member_names,
--- manifests/install.pp
+++ manifests/install.pp
@@ -7,20 +7,45 @@ class profile_haproxy::install {
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
+
+  # add haproxy ppa repo
   include apt
   apt::ppa { 'ppa:vbernat/haproxy-1.6': }
+
   if $::monitor_address != undef {
-    class { 'haproxy':
-      package_ensure => 'latest',
-      global_options => {
-        'log' => "${::monitor_address} local0",
-      },
-      merge_options  => true,
-    }
+    $logserver =  "${::monitor_address} local0"
   }
   else {
-    class { 'haproxy':
-      package_ensure => 'latest',
-    }
+    $logserver = '127.0.0.1 local0'
   }
+
+  class { 'haproxy':
+    package_ensure   => 'latest',
+    global_options   => {
+      'log'                        => $logserver,
+      'tune.ssl.default-dh-param'  => '2048',
+      'ssl-default-bind-options'   => 'no-sslv3',
+      'ssl-default-bind-ciphers'   => 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:RSA+AESGCM:RSA+AES:!aNULL:!MD5:!DSS',
+      'ssl-default-server-option'  => 'no-sslv3',
+      'ssl-default-server-ciphers' => 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:RSA+AESGCM:RSA+AES:!aNULL:!MD5:!DSS',
+    },
+    defaults_options => {
+      'log'     => 'global',
+      'retries' => '3',
+      'option'  => [
+        'redispatch',
+        'http-server-close',
+      ],
+      'timeout' => [
+        'http-request 10s',
+        'connect 3s',
+        'client 1m',
+        'server 1m',
+        'check 10s',
+      ],
+      'maxconn' => '8000',
+    },
+    merge_options    => true,
+  }
+
 }
